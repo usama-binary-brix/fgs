42b631d6b7f3fb583750f49ca347e49d5616185f
dev016binarybrix <dev003.tinglegames@gmail.com>
2025-03-18 23:13:52 +0500

    update with new changes

--- src/app/(admin)/dashboard/leads/components/LeadsTable.tsx
+++ src/app/(admin)/dashboard/leads/components/LeadsTable.tsx
@@ -1,12 +1,14 @@
 
 'use client'
 
+import TopButtons from '@/components/Buttons/TopButtons';
 import { Dropdown } from '@/components/ui/dropdown/Dropdown';
 import { DropdownItem } from '@/components/ui/dropdown/DropdownItem';
 import { Table, TableBody, TableCell, TableHeader, TableRow } from '@/components/ui/table';
 import { MoreDotIcon } from '@/icons';
 import Image from 'next/image';
 import { useState } from 'react';
+import { IoSearchOutline } from 'react-icons/io5';
 
 interface Lead {
   id: string;
@@ -39,6 +41,29 @@ const LeadsTable = () => {
       setIsOpen(false);
     }
   return (
+   <>
+    <div className="flex justify-between items-center mb-3">
+    <div className="inline-flex items-center gap-3">
+       <div className="hidden sm:block">
+         <div className="flex items-center space-x-2">
+           <div className="relative">
+           <IoSearchOutline className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+             <input
+               className="text-xs border rounded-lg pl-9 pr-2 h-9 w-64 border-gray-300 focus:border-gray-400 focus:outline-none"
+               placeholder="Search"
+             />
+           </div>
+         </div>
+       </div>
+     </div>
+
+     <div className="flex items-center gap-3">
+         <TopButtons label="Edit Columns" variant="outlined" />
+         <TopButtons label="Filters" variant="outlined" />
+         <TopButtons label="Add New Account" variant="primary" />
+    
+       </div>
+    </div>
     <div className="overflow-hidden rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
       <div className="max-w-full overflow-x-auto">
         <Table>
@@ -96,6 +121,7 @@ const LeadsTable = () => {
         </Table>
       </div>
     </div>
+   </>
   );
 };
 
--- src/app/(admin)/dashboard/leads/page.tsx
+++ src/app/(admin)/dashboard/leads/page.tsx
@@ -15,35 +15,6 @@ const Page = () => {
   return (
     <>
       <PageTitle title="All Leads" />
-
-     <div className="flex justify-between items-center mb-3">
-     <div className="inline-flex items-center gap-3">
-        <div className="hidden sm:block">
-          <div className="flex items-center space-x-2">
-            <div className="relative">
-            <IoSearchOutline className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-              <input
-                className="text-xs border rounded-lg pl-9 pr-2 h-9 w-64 border-gray-300 focus:border-gray-400 focus:outline-none"
-                placeholder="Search"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex items-center gap-3">
-          <TopButtons label="Edit Columns" variant="outlined" />
-          <TopButtons label="Filters" variant="outlined" />
-          <TopButtons label="Add New Account" variant="primary" />
-     
-        </div>
-     </div>
-
-
-
-
-
-
       <LeadsTable />
     </>
   );
