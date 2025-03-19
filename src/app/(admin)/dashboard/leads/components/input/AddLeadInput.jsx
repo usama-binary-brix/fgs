621a1beff5cf4364f8b37689234c36ec00049438
dev003.binarybrix@gmail.com <dev003.binarybrix@gmail.com>
2025-03-19 17:34:24 +0500

    create add new lead page

--- src/app/(admin)/dashboard/leads/components/input/AddLeadInput.jsx
+++ src/app/(admin)/dashboard/leads/components/input/AddLeadInput.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AddLeadInput = ({ label, type = "text", placeholder = "---", ...props }) => {
   return (
     <div className="w-full">
-      <label className="text-xs text-gray-500 font-medium" htmlFor={label}>
+      <label className="text-xs text-customGray font-normal" htmlFor={label}>
         {label}
       {console.log(label)}
       </label>
@@ -11,7 +11,7 @@ const AddLeadInput = ({ label, type = "text", placeholder = "---", ...props }) =
         id={label}
         type={type}
         placeholder={placeholder}
-        className="w-full  px-2 rounded-sm border border-gray-300 mt-1 outline-none text-md"
+        className="w-full  px-2 py-1 text-darkGray rounded-sm border border-gray-300 mt-1 outline-none text-md"
         {...props}
 
        
